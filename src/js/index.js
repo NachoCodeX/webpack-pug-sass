94d4784ddb3a59517c54486931a00dd9d7fe217d
Nacho Castillo <4811095288ic@gmail.com>
2019-03-12 11:07:06 -0600

    typescript support [added]

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,1 @@
-const styles = require('../styles/styles.scss');
-
-// import '../pages/text-styles.pug';
-// import '../test.pug';
-// import '../pages/links.pug';
-// import '../pages/images.pug';
-// import '../pages/forms.pug';
+const styles = require("../styles/styles.scss");
--- webpack.config.js
+++ webpack.config.js
@@ -1,117 +1,97 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 function createHtmlWebpackPlugin(array, folder) {
-    const newArray = array.map((_, index) => {
+  const newArray = array.map((_, index) => {
+    return _.map(
+      page =>
+        new HtmlWebpackPlugin({
+          title: page.title,
+          inject: "body",
+          template: `src/pages/${folder}/u${index + 1}/${page.filename}.pug`,
+          filename: `pages/${folder}/u${index + 1}/${page.filename}.html`
+        })
+    );
+  });
 
-        return _.map((page) => new HtmlWebpackPlugin({
-            title: page.title,
-            inject: 'body',
-            template: `src/pages/${folder}/u${index + 1}/${page.filename}.pug`,
-            filename: `pages/${folder}/u${index + 1}/${page.filename}.html`
-        }));
-    });
-
-    return [].concat(...newArray)
+  return [].concat(...newArray);
 }
-
 
 const options = {
-    rootDir: process.cwd(),
-    devBuild: process.env.NODE_ENV !== 'production',
-}
+  rootDir: process.cwd(),
+  devBuild: process.env.NODE_ENV !== "production"
+};
 
-
-
-const pages = [
-
-    { filename: 'homeworks', title: 'Homeworks' },
-
-],
-    classExercisesPages = [
-        [
-            { filename: 'forms', title: 'Forms' },
-            { filename: 'links', title: 'Links' },
-            { filename: 'images', title: 'Images' },
-            { filename: 'text-styles', title: 'Text styles' },
-            { filename: 'tables', title: 'Tables' },
-
-        ]
-    ],
-    homeworkPages = [
-        [{ filename: 'homework1', title: 'Style Matrix' },
-        { filename: 'homework2', title: 'Curriculum' },
-        { filename: 'homework3', title: 'Schedule' }
-
-        ]
-    ];
-
-
-
+const pages = [{ filename: "homeworks", title: "Homeworks" }],
+  classExercisesPages = [
+    [
+      { filename: "forms", title: "Forms" },
+      { filename: "links", title: "Links" },
+      { filename: "images", title: "Images" },
+      { filename: "text-styles", title: "Text styles" },
+      { filename: "tables", title: "Tables" }
+    ]
+  ],
+  homeworkPages = [
+    [
+      { filename: "homework1", title: "Style Matrix" },
+      { filename: "homework2", title: "Curriculum" },
+      { filename: "homework3", title: "Schedule" }
+    ]
+  ];
 
 const scssLoader = {
     test: /\.scss$/,
-    use: ['style-loader', 'css-loader', 'sass-loader'],
+    use: ["style-loader", "css-loader", "sass-loader"],
     exclude: /(node_module)/
-}, pugLoader = {
+  },
+  pugLoader = {
     test: /\.pug$/,
-    use: ['html-loader?minimize=false', 'pug-html-loader?pretty=true'],
-
-};
-
-
+    use: ["html-loader?minimize=false", "pug-html-loader?pretty=true"]
+  },
+  tsLoader = {
+    test: /\.ts$/,
+    use: ["ts-loader"]
+  };
 
 const cfg = {
-    node: {
-        __dirname: true
-    },
-    stats: "errors-only",
-    target: 'web',
-    // externals: [nodeExternals()],
-    mode: 'development',
-    entry: {
-        app: path.join(__dirname, 'src', 'js', 'index.js')
-    },
-    module: {
-        rules: [
-            scssLoader,
-            pugLoader
-        ]
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].bundle.js'
-    },
-    plugins: [
+  node: {
+    __dirname: true
+  },
+  stats: "errors-only",
+  target: "web",
+  // externals: [nodeExternals()],
+  mode: "development",
+  entry: {
+    app: path.join(__dirname, "src", "js", "index.js")
+  },
+  module: {
+    rules: [scssLoader, pugLoader, tsLoader]
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "NachoCodeX",
+      inject: "body",
+      template: "src/index.pug"
+    }),
+    ...pages.map(
+      page =>
         new HtmlWebpackPlugin({
-            title: 'NachoCodeX',
-            inject: 'body',
-            template: 'src/index.pug',
-        }),
-        ...pages.map(page => new HtmlWebpackPlugin({
-            title: page.title,
-            inject: 'body',
-            template: `src/pages/${page.filename}.pug`,
-            filename: `pages/${page.filename}.html`
-        })),
-        ...createHtmlWebpackPlugin(classExercisesPages, 'class-exercises'),
-        ...createHtmlWebpackPlugin(homeworkPages, 'homeworks'),
-        new CopyWebpackPlugin([
-            { from: 'src/img', to: 'public/img' }
-        ]),
+          title: page.title,
+          inject: "body",
+          template: `src/pages/${page.filename}.pug`,
+          filename: `pages/${page.filename}.html`
+        })
+    ),
+    ...createHtmlWebpackPlugin(classExercisesPages, "class-exercises"),
+    ...createHtmlWebpackPlugin(homeworkPages, "homeworks"),
+    new CopyWebpackPlugin([{ from: "src/img", to: "public/img" }])
+  ]
+};
 
-
-
-    ]
-
-
-
-
-
-}
-
-
-
-module.exports = cfg
+module.exports = cfg;
