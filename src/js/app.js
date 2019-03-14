b23758a7d03efaa881f530e40e64f1d18d4220d9
Nacho Castillo <4811095288ic@gmail.com>
2019-03-14 12:59:13 -0600

    last version

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,143 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-        .register('./js/sw.js', { scope: '../' })
-        .then(function (registration) {
-            console.log("Service Worker registered", registration);
+'use strict';
 
-        })
-        .catch(function (e) {
-            console.log("Service worker failed to register", e);
+class SieveOFEratosthenes {
+  constructor() {
+    this.init();
+    this.initListeners();
+    this.total = parseInt(this.getElementValue('sieve-input'));
+    this.board = this.getElementById('sieve-board');
+    this.elements = this.generateElements();
+    this.appendToBoard();
+  }
 
-        });
+  resetElementsStyles() {
+    for (const e of this.elements) {
+      e.style.backgroundColor = '#ffdd78';
+      e.style.color = '#212121';
+    }
+  }
+
+  multipliesOf(n) {
+    this.resetElementsStyles();
+    for (const e of this.elements) {
+      const value = parseInt(e.getAttribute('value'));
+      // console.log();
+      if (value % n == 0) {
+        console.log(`${value} is multiple of ${n}`);
+        e.style.background = '#212121';
+        e.style.color = '#fff';
+      }
+    }
+  }
+  onAction(e) {
+    const type = parseInt(e.target.getAttribute('data-type'));
+    this.multipliesOf(type);
+  }
+  onPrime() {
+    this.resetElementsStyles();
+    const aux = [];
+    for (const e of this.elements) {
+      const value = parseInt(e.getAttribute('value'));
+      if (value === 1 || aux.indexOf(value) !== -1) continue;
+
+      for (let i = value + 1; i <= this.elements.length; i++) {
+        if (i % value == 0) aux.push(i);
+      }
+      // primes.push(value);
+      e.style.background = '#212121';
+      e.style.color = '#fff';
+    }
+
+    // console.log(aux);
+    // console.log(primes);
+  }
+  init() {
+    this.input = this.getElementById('sieve-input');
+    this.btns = document.getElementsByName('action').forEach(btn => {
+      btn.addEventListener('click', this.onAction.bind(this));
+    });
+
+    this.clear = this.getElementById('clear');
+    this.prime = this.getElementById('prime');
+    console.log(this.btns);
+  }
+  initListeners() {
+    this.input.addEventListener('input', _.debounce(this.onChanges.bind(this), 500));
+    this.clear.addEventListener('click', this.onClear.bind(this));
+    this.prime.addEventListener('click', this.onPrime.bind(this));
+  }
+
+  onClear() {
+    console.log('CLEAR');
+    this.resetElementsStyles();
+  }
+  onChanges(event) {
+    if (event.target.value === '') return;
+    this.total = parseInt(event.target.value);
+    if (this.total < 20) {
+      this.total = 20;
+      alert(`I SAID DON'T BE STUPID!`);
+      this.setElementValue('sieve-input', this.total);
+      return;
+    } else if (this.total > 150) {
+      this.total = 150;
+      alert(`I SAID DON'T BE STUPID!`);
+      this.setElementValue('sieve-input', this.total);
+      return;
+    }
+
+    this.elements = this.generateElements();
+    this.clearBoard();
+    this.generateGridStyles();
+    this.appendToBoard();
+  }
+
+  generateGridStyles() {
+    this.board.style.display = 'grid';
+    // this.board.style.gridAutoRows = '4rem';
+    if (this.total >= 20 && this.total < 30) this.board.style.gridTemplateColumns = `repeat(5,1fr)`;
+    else if (this.total >= 30 && this.total < 40) this.board.style.gridTemplateColumns = `repeat(6,1fr)`;
+    else if (this.total >= 40 && this.total < 50) this.board.style.gridTemplateColumns = `repeat(8,1fr)`;
+    else {
+      this.board.style.gridTemplateColumns = `repeat(10,1fr)`;
+    }
+    // this.board.style.gridAutoRows = '10rem';
+    // this.board.style.gridAutoColumns = '10rem';
+  }
+
+  clearBoard() {
+    this.board.innerHTML = '';
+  }
+  appendToBoard() {
+    for (const element of this.elements) {
+      // console.log(element);
+      this.board.appendChild(element);
+    }
+  }
+
+  generateElements() {
+    const elements = [];
+    for (let i = 0; i < this.total; i++) {
+      const element = document.createElement('div');
+      element.className = 'sieve-item';
+      element.innerHTML = i + 1;
+
+      element.setAttribute('value', i + 1);
+      elements.push(element);
+    }
+    // console.log(this.elements);
+    return elements;
+  }
+
+  setElementValue(id, value) {
+    document.getElementById(id).value = value;
+  }
+  getElementValue(id) {
+    return document.getElementById(id).value;
+  }
+  getElementById(id) {
+    return document.getElementById(id);
+  }
 }
+
+new SieveOFEratosthenes();
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const nodeExternals = require('webpack-node-externals');
 
 function createHtmlWebpackPlugin(array, folder) {
   const newArray = array.map((_, index) => {
@@ -8,7 +9,7 @@ function createHtmlWebpackPlugin(array, folder) {
       page =>
         new HtmlWebpackPlugin({
           title: page.title,
-          inject: "body",
+          inject: 'body',
           template: `src/pages/${folder}/u${index + 1}/${page.filename}.pug`,
           filename: `pages/${folder}/u${index + 1}/${page.filename}.html`
         })
@@ -20,77 +21,79 @@ function createHtmlWebpackPlugin(array, folder) {
 
 const options = {
   rootDir: process.cwd(),
-  devBuild: process.env.NODE_ENV !== "production"
+  devBuild: process.env.NODE_ENV !== 'production'
 };
 
-const pages = [{ filename: "homeworks", title: "Homeworks" }],
+const pages = [{ filename: 'homeworks', title: 'Homeworks' }],
   classExercisesPages = [
     [
-      { filename: "forms", title: "Forms" },
-      { filename: "links", title: "Links" },
-      { filename: "images", title: "Images" },
-      { filename: "text-styles", title: "Text styles" },
-      { filename: "tables", title: "Tables" }
+      { filename: 'forms', title: 'Forms' },
+      { filename: 'links', title: 'Links' },
+      { filename: 'images', title: 'Images' },
+      { filename: 'text-styles', title: 'Text styles' },
+      { filename: 'tables', title: 'Tables' }
     ]
   ],
   homeworkPages = [
     [
-      { filename: "homework1", title: "Style Matrix" },
-      { filename: "homework2", title: "Curriculum" },
-      { filename: "homework3", title: "Schedule" }
-    ]
+      { filename: 'homework1', title: 'Style Matrix' },
+      { filename: 'homework2', title: 'Curriculum' },
+      { filename: 'homework3', title: 'Schedule' }
+    ],
+    [{ filename: 'homework1', title: 'Criba de Eratóstenes' }]
   ];
 
 const scssLoader = {
     test: /\.scss$/,
-    use: ["style-loader", "css-loader", "sass-loader"],
+    use: ['style-loader', 'css-loader', 'sass-loader'],
     exclude: /(node_module)/
   },
   pugLoader = {
     test: /\.pug$/,
-    use: ["html-loader?minimize=false", "pug-html-loader?pretty=true"]
+    use: ['html-loader?minimize=false', 'pug-html-loader?pretty=true']
   },
   tsLoader = {
     test: /\.ts$/,
-    use: ["ts-loader"]
+    use: ['ts-loader']
   };
 
 const cfg = {
   node: {
     __dirname: true
   },
-  stats: "errors-only",
-  target: "web",
   // externals: [nodeExternals()],
-  mode: "development",
+  stats: 'errors-only',
+  target: 'web',
+  mode: 'development',
   entry: {
-    app: path.join(__dirname, "src", "js", "index.js")
+    app: path.join(__dirname, 'src', 'index.js')
   },
   module: {
     rules: [scssLoader, pugLoader, tsLoader]
   },
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].bundle.js"
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js'
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "NachoCodeX",
-      inject: "body",
-      template: "src/index.pug"
+      title: 'NachoCodeX',
+      inject: 'body',
+      template: 'src/index.pug'
     }),
     ...pages.map(
       page =>
         new HtmlWebpackPlugin({
           title: page.title,
-          inject: "body",
+          inject: 'body',
           template: `src/pages/${page.filename}.pug`,
           filename: `pages/${page.filename}.html`
         })
     ),
-    ...createHtmlWebpackPlugin(classExercisesPages, "class-exercises"),
-    ...createHtmlWebpackPlugin(homeworkPages, "homeworks"),
-    new CopyWebpackPlugin([{ from: "src/img", to: "public/img" }])
+    ...createHtmlWebpackPlugin(classExercisesPages, 'class-exercises'),
+    ...createHtmlWebpackPlugin(homeworkPages, 'homeworks'),
+    new CopyWebpackPlugin([{ from: 'src/img', to: 'public/img' }]),
+    new CopyWebpackPlugin([{ from: 'src/js', to: 'js' }])
   ]
 };
 
